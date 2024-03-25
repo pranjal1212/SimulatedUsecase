23b89cc9ebd34ae3b199f7a4aa1cd813bcd3ab6c
Anusha Sahi <anusha_mithapani@persistent.com>
2024-03-25 21:43:04 +0530

    LogIn-Register added

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,8 @@ CORS(app)
 
 def connection():
    # Connect to DynamoDB
-   dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', aws_access_key_id='', aws_secret_access_key='',endpoint_url='https://dynamodb.ap-south-1.amazonaws.com')
-   table_name = 'master_gym'
+   dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', aws_access_key_id='AKIAXYKJVS2ZY5TFNOQZ', aws_secret_access_key='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',endpoint_url='https://dynamodb.ap-south-1.amazonaws.com')
+   table_name = 'master'
    table = dynamodb.Table(table_name)
    return table
 
@@ -26,18 +26,123 @@ def home():
        print(item)
    return {"response" : items}
 
+
+@app.route("/duplicate")
+def getDuplicate():
+   table = connection()
+   firstName = request.args.get('First_name')
+   lastName = request.args.get('Last_name')
+   birthDate = request.args.get('Birth_Date')
+   categoryType = request.args.get('Category_Type')
+   gymId = request.args.get('Gym_ID')
+   city = request.args.get('City')
+   stateVal = request.args.get('State')
+   country = request.args.get('Country')
+   pincode = request.args.get('Pincode')
+   response = table.scan(
+      FilterExpression="first_name = :first_name_val AND last_name = :last_name_val AND birth_dt = :birth_val AND category_type = :category_val AND gym_id = :gym_val AND city = :city_val AND #s =:state_val AND country =:country_val AND pincode = :pincode_val",
+      ExpressionAttributeNames={
+         '#s' : 'state'
+      },
+      ExpressionAttributeValues={
+         ":first_name_val": firstName,
+         ":last_name_val": lastName,
+         ":birth_val" : birthDate,
+         ":category_val" : categoryType,
+         ":gym_val" : gymId,
+         ":city_val" : city,
+         ":state_val": stateVal,
+         ":country_val": country,
+         ":pincode_val": pincode
+      }
+   )
+   if response["Count"] > 0:
+      return response
+   else:
+      return "None"
+   
+@app.route("/duplicateCustomer")
+def getDuplicateCus():
+   table = connection()
+   firstName = request.args.get('First_name')
+   lastName = request.args.get('Last_name')
+   birthDate = request.args.get('Birth_Date')
+   categoryType = request.args.get('Category_Type')
+   gymId = request.args.get('Gym_ID')
+   city = request.args.get('City')
+   stateVal = request.args.get('State')
+   country = request.args.get('Country')
+   pincode = request.args.get('Pincode')
+   response = table.scan(
+      FilterExpression="first_name = :first_name_val AND last_name = :last_name_val AND birth_dt = :birth_val AND  city = :city_val AND #s =:state_val AND country =:country_val AND pincode = :pincode_val",
+      ExpressionAttributeNames={
+         '#s' : 'state'
+      },
+      ExpressionAttributeValues={
+         ":first_name_val": firstName,
+         ":last_name_val": lastName,
+         ":birth_val" : birthDate,
+         ":city_val" : city,
+         ":state_val": stateVal,
+         ":country_val": country,
+         ":pincode_val": pincode
+      }
+   )
+   if response["Count"] > 0:
+      return response["Items"][0]["customer_id"]
+   else:
+      return "None"
+   
+
+def checkDuplicate(table, firstName, lastName, birthDate, categoryType, gymId, city, stateVal, country, pincode):
+   response = table.scan(
+      FilterExpression="first_name = :first_name_val AND last_name = :last_name_val AND birth_dt = :birth_val AND category_type = :category_val AND gym_id = :gym_val AND city = :city_val AND #s =:state_val AND country =:country_val AND pincode = :pincode_val",
+      ExpressionAttributeNames={
+         '#s' : 'state'
+      },
+      ExpressionAttributeValues={
+         ":first_name_val": firstName,
+         ":last_name_val": lastName,
+         ":birth_val" : birthDate,
+         ":category_val" : categoryType,
+         ":gym_val" : gymId,
+         ":city_val" : city,
+         ":state_val": stateVal,
+         ":country_val": country,
+         ":pincode_val": pincode
+      }
+   )
+   print(response)
+   if response["Count"] > 0:
+      return True
+   else:
+      return False
+
 @app.route("/register" , methods = ['POST'])
 def register():
    if(request.data):
-      
-      customer_id = random.randint(100000, 999999)
-      print(customer_id)
+      table = connection()
       response = request.get_json()
       print(response)
-      response["customer_id"] = f"{customer_id}"
-      table = connection()
-      table.put_item(Item = response)
-      return {"response" : response}
+      result = checkDuplicate(table, response["first_name"] , response["last_name"], response["birth_dt"], response["category_type"], response["gym_id"], response["city"], response["state"], response["country"], response["pincode"])
+
+      if result == False:
+         customer_id = str(random.randint(100000, 999999))
+         # customer_id = "969650"
+         print(customer_id)
+         while True:
+            response1 = table.get_item(Key = {"customer_id": customer_id})
+            if 'Item' in response1:
+               customer_id = str(random.randint(100000, 999999))
+            else:
+               break
+         print(customer_id)
+         
+         response["customer_id"] = f"{customer_id}"
+         table.put_item(Item = response)
+         return {"response" : response}
+      else:
+         return {"response" : "Duplicate"}
    else:
       return {"response" : "No Data Found"}
 
@@ -46,10 +151,38 @@ def register():
 def getById():
    table = connection()
    customerId = request.args.get('Customer_ID') 
+   print(type(customerId))
    response = table.get_item(Key = {"customer_id": customerId})
    return {"response" : response["Item"]}
 
+@app.route('/login', methods=['POST'])
+def login():
+   table = connection()
+   data = request.get_json()
+   first_name = data.get('firstName')
+   last_name = data.get('lastName')
+   birth_dt = data.get('dob')
+   pincode = data.get('pincode')
    
+   response = table.scan(
+       FilterExpression='first_name = :fName and last_name = :lName and birth_dt = :dob and pincode = :pincode',
+       ExpressionAttributeValues={
+           ':fName': first_name,
+           ':lName': last_name,
+           ':dob': birth_dt,
+           ':pincode': pincode
+       }
+   )
+
+   count = response['Count']
+   if 'Items in response' and count > 0:
+       
+       #user_data = response['Items'][0]  # Assuming only one user is found
+       return jsonify({'success': True, 'message': 'Login successfull'})
+
+   else:
+       return jsonify({'success': False, 'message': 'User not found or incorrect details'})
+ 
                   
 if __name__ == '__main__':
    app.run(debug=True)
---
+++ frontend/src/login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Log.css';
+
+const Login = ({handleRegister}) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [dob, setDob] = useState('');
+    const [pincode, setPincode] = useState('');
+    const [message, setMessage] = useState('');
+    //const [userData, setUserData] = useState(null)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/login', { firstName, lastName, dob, pincode });
+            const { success, message} = response.data;
+            if (success) {
+                // Login successful, do something with user_data
+                setMessage(message);
+
+            } else {
+                setMessage(message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setMessage('An error occurred. Please try again later.');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit} className="login-form">
+                <label>First Name:</label>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+                <label>Last Name:</label>
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+                <label>Date of Birth:</label>
+                <input type="text" value={dob} onChange={(e) => setDob(e.target.value)} required />
+                <label>Pincode:</label>
+                <input type="text" value={pincode} onChange={(e) => setPincode(e.target.value)} required />
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account?  <a href='#' onClick={() => handleRegister()}>Register</a></p>
+            {message && <p>{message}</p>}
+        </div>
+    );
+};
+export default Login;
